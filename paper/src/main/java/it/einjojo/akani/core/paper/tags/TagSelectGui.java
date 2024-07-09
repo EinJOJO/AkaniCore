698d925482a66f13a6b230a58752416bf67de899
einjojo <einjojo@proton.me>
2024-07-09 03:06:36 +0200

    ui things

--- paper/src/main/java/it/einjojo/akani/core/paper/tags/TagSelectGui.java
+++ paper/src/main/java/it/einjojo/akani/core/paper/tags/TagSelectGui.java
@@ -6,13 +6,20 @@ import it.einjojo.akani.core.api.tags.TagManager;
 import mc.obliviate.inventory.Gui;
 import mc.obliviate.inventory.Icon;
 import mc.obliviate.inventory.pagination.PaginationManager;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.inventory.ItemFlag;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 public class TagSelectGui extends Gui {
     private static final String TITLE = "§cWähle einen Tag aus";
@@ -33,19 +40,35 @@ public class TagSelectGui extends Gui {
         for (Tag tag : tagManager.availableTags()) {
             Icon icon = new Icon(Material.NAME_TAG).toComp()
                     .setName(tag.displayText())
+                    .setLore(List.of(
+                            Component.empty(),
+                            Component.text(tag.lore(), NamedTextColor.GRAY).decoration(TextDecoration.ITALIC, false),
+                            Component.empty(),
+                            Component.text("Auswählen", NamedTextColor.GREEN)
+                    ))
                     .toIcon();
             if (tag.equals(tagHolder.selectedTag())) {
                 icon.enchant(Enchantment.ARROW_DAMAGE);
                 icon.hideFlags(ItemFlag.HIDE_ENCHANTS);
+            } else {
+                icon.onClick((clickEvent -> {
+                    onSelectTag(clickEvent, tag);
+                }));
             }
             paginationManager.addItem(icon);
         }
     }
 
+    private void onSelectTag(InventoryClickEvent event, Tag tag) {
+        open();
+        tagHolder.setSelectedTag(tag);
+        player.playSound(player, Sound.UI_BUTTON_CLICK, 1, 1.2f);
+    }
+
     @Override
     public void onClose(InventoryCloseEvent event) {
         super.onClose(event);
-        player.sendMessage(event.getReason().name());
+        player.sendMessage(event.getReason().name()); //TODO
         if (event.getReason().equals(InventoryCloseEvent.Reason.OPEN_NEW)) {
             return;
         }
